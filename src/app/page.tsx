19ee671a151fad7c7c37717b673487e673406365
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-03-13 13:01:23 +0530

    feat: enhance homepage with navigation links for login and signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex justify-center w-full h-screen mt-72">
-      <h1 className="text-3xl font-bold">Hello 👋</h1>
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <h1 className="text-3xl font-bold m-8">Hello 👋</h1>
+      <div className="flex gap-12">
+        <Link
+          href="/login"
+          className="text-xl font-bold p-4 bg-blue-500 rounded-lg"
+        >
+          Login
+        </Link>
+        <Link
+          href="/signup"
+          className="text-xl font-bold p-4 bg-blue-500 rounded-lg"
+        >
+          Signup
+        </Link>
+      </div>
     </div>
   );
 }
